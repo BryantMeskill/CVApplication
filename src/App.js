99ebd17f6ddcba7f540be213fbf8da670902fb9e
Bryant Meskill <bryant.meskill@gmail.com>
2022-04-05 07:28:47 -0500

    Basic pathing added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from "react";
+import General from "./components/General";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import "./styles/app.css";
 
-class App extends React.Component {}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 id="title">Curriculum Vitae</h1>
+        {/* <General />
+        <Education />
+        <Experience /> */}
+      </div>
+    );
+  }
+}
 
 export default App;
