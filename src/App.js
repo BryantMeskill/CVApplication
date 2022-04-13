712fe48ccb489d3a671e339c5c98dfcafc7af831
Bryant Meskill <bryant.meskill@gmail.com>
2022-04-13 11:53:52 -0500

    Refactored to functional components.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import General from "./components/General";
 import Education from "./components/Education";
@@ -6,36 +6,65 @@ import Experience from "./components/Experience";
 import Footer from "./components/Footer";
 import "./styles/app.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isEditing: true,
-    };
-    this.toggleIsEditing.bind(this);
-  }
+const App = () => {
+  const [isEditing, setIsEditing] = useState(true);
 
-  toggleIsEditing = () => {
-    this.setState((prevState) => ({
-      isEditing: !prevState.isEditing,
-    }));
+  const toggleIsEditing = () => {
+    setIsEditing(!isEditing);
   };
 
-  render() {
-    const { isEditing } = this.state;
-    return (
-      <div className="section">
-        <Header />
-        <General isEditing={isEditing} />
-        <Education isEditing={isEditing} />
-        <Experience isEditing={isEditing} />
-        <button className="submit" onClick={this.toggleIsEditing}>
-          {isEditing ? "Save" : "Edit"}
-        </button>
-        <Footer />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="section">
+      <Header />
+      <General isEditing={isEditing} />
+      <Education isEditing={isEditing} />
+      <Experience isEditing={isEditing} />
+      <button className="submit" onClick={toggleIsEditing}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+      <Footer />
+    </div>
+  );
+};
+
+// import React from "react";
+// import Header from "./components/Header";
+// import General from "./components/General";
+// import Education from "./components/Education";
+// import Experience from "./components/Experience";
+// import Footer from "./components/Footer";
+// import "./styles/app.css";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isEditing: true,
+//     };
+//     this.toggleIsEditing.bind(this);
+//   }
+
+//   toggleIsEditing = () => {
+//     this.setState((prevState) => ({
+//       isEditing: !prevState.isEditing,
+//     }));
+//   };
+
+//   render() {
+//     const { isEditing } = this.state;
+//     return (
+//       <div className="section">
+//         <Header />
+//         <General isEditing={isEditing} />
+//         <Education isEditing={isEditing} />
+//         <Experience isEditing={isEditing} />
+//         <button className="submit" onClick={this.toggleIsEditing}>
+//           {isEditing ? "Save" : "Edit"}
+//         </button>
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
