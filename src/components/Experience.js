e5a2943a039be42a9c3cd7679f581e9d6bea5a2b
Bryant Meskill <bryant.meskill@gmail.com>
2022-04-14 03:47:35 -0500

    Switched to functional components.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,62 +1,127 @@
-//company name, position title, main tasks of your jobs,
-//date from and until when you worked for that company
-import React from "react";
-class Experience extends React.Component {
-  constructor(props) {
-    super(props);
+import React, { useState } from "react";
 
-    this.state = { company: "", position: "", tasks: "", worked: "" };
+const Experience = ({ isEditing }) => {
+  const [formData, setFormData] = useState({
+    company: "",
+    position: "",
+    tasks: "",
+    worked: "",
+  });
 
-    this.handleChange.bind(this);
-  }
+  const handleFormChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  handleChange = (e) => {};
-
-  render() {
-    const { isEditing } = this.props;
-    const { company, position, tasks, worked } = this.state;
-    return (
-      <div className="section">
-        <h1 className="subtitle">Experience</h1>
-        <form>
-          {isEditing ? (
-            <div className="inputContainer">
-              <label htmlFor="company">Company Name:</label>
-              <input
-                className="company"
-                value={company.value}
-                onChange={this.handleChange}
-              />
-              <label htmlFor="position">Positon/Title:</label>
-              <input
-                className="position"
-                value={position.value}
-                onChange={this.handleChange}
-              />
-              <label htmlFor="tasks">Job Duties:</label>
-              <input
-                className="tasks"
-                value={tasks.value}
-                onChange={this.handleChange}
-              />
-              <label htmlFor="worked">Year(s) Worked:</label>
-              <input
-                className="worked"
-                value={worked.value}
-                onChange={this.handleChange}
-              />
-            </div>
-          ) : (
-            <div className="userInput experience">
-              <text>Company Name: {company}</text>
-              <text>Position/Title: {position}</text>
-              <text>Job Duties: {tasks}</text>
-              <text>Year(s) Worked: {worked}</text>
-            </div>
-          )}
+  return (
+    <div className="section">
+      <h1 className="subtitle">Experience</h1>
+      {isEditing ? (
+        <form className="inputContainer">
+          <label htmlFor="company">Company Name:</label>
+          <input
+            name="company"
+            className="company"
+            value={formData.company}
+            onChange={handleFormChange}
+          />
+          <label htmlFor="position">Position/Title: </label>
+          <input
+            name="position"
+            className="position"
+            value={formData.position}
+            onChange={handleFormChange}
+          />
+          <label htmlFor="tasks">Job Duties: </label>
+          <input
+            name="tasks"
+            className="tasks"
+            value={formData.tasks}
+            onChange={handleFormChange}
+          />
+          <label htmlFor="worked">Year(s) Worked: </label>
+          <input
+            name="worked"
+            className="worked"
+            value={formData.worked}
+            onChange={handleFormChange}
+          />
         </form>
-      </div>
-    );
-  }
-}
+      ) : (
+        <div className="userInput experience">
+          <span>Company Name: {formData.company}</span>
+          <span>Position/Title: {formData.position}</span>
+          <span>Job Duties: {formData.tasks}</span>
+          <span>Year(s) Worked: {formData.worked}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Experience;
+
+//company name, position title, main tasks of your jobs,
+// //date from and until when you worked for that company
+// import React from "react";
+// class Experience extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = { company: "", position: "", tasks: "", worked: "" };
+
+//     this.handleChange.bind(this);
+//   }
+
+//   handleChange = (e) => {};
+
+//   render() {
+//     const { isEditing } = this.props;
+//     const { company, position, tasks, worked } = this.state;
+//     return (
+//       <div className="section">
+//         <h1 className="subtitle">Experience</h1>
+//         <form>
+//           {isEditing ? (
+//             <div className="inputContainer">
+//               <label htmlFor="company">Company Name:</label>
+//               <input
+//                 className="company"
+//                 value={company.value}
+//                 onChange={this.handleChange}
+//               />
+//               <label htmlFor="position">Positon/Title:</label>
+//               <input
+//                 className="position"
+//                 value={position.value}
+//                 onChange={this.handleChange}
+//               />
+//               <label htmlFor="tasks">Job Duties:</label>
+//               <input
+//                 className="tasks"
+//                 value={tasks.value}
+//                 onChange={this.handleChange}
+//               />
+//               <label htmlFor="worked">Year(s) Worked:</label>
+//               <input
+//                 className="worked"
+//                 value={worked.value}
+//                 onChange={this.handleChange}
+//               />
+//             </div>
+//           ) : (
+//             <div className="userInput experience">
+//               <text>Company Name: {company}</text>
+//               <text>Position/Title: {position}</text>
+//               <text>Job Duties: {tasks}</text>
+//               <text>Year(s) Worked: {worked}</text>
+//             </div>
+//           )}
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+// export default Experience;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,28 @@
 import React from "react";
 import logo from "../img/github.png";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className="footerDiv">
-        <text>
-          <img src={logo} alt="GitHub logo" />
-          /BryantMeskill
-        </text>
-      </div>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <div className="footerDiv">
+      <span>
+        <img src={logo} alt="Github Logo" />
+        /BryantMeskill
+      </span>
+    </div>
+  );
+};
+
+// class Footer extends React.Component {
+//   render() {
+//     return (
+//       <div className="footerDiv">
+//         <text>
+//           <img src={logo} alt="GitHub logo" />
+//           /BryantMeskill
+//         </text>
+//       </div>
+//     );
+//   }
+// }
 
 export default Footer;
